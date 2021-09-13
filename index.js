cba870421dae77710c06e80044cc4af57645c0d2
Kyle Samuel <Ksamuel92@gmail.com>
2021-09-13 16:09:43 -0400

    commit tree af49e0c0af91ff41b0f5ab419df38a2b986e94ab

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize (string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize (string) {
+    return  string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize (string) {
+  const arrayOfWords = string.split(" ");
+  const titleizedArray = arrayOfWords.map((word, index) => {
+    if (!(this.isInvalidForTitleize(word)) || (index === 0) ){
+      return this.capitalize(word)
+    } else {
+      return word
+    }
+  })
+  debugger
+  return titleizedArray.join(" ")
+}
+
+
+ static isInvalidForTitleize (word) {
+   const forbiddenWords = ["a", "an", "but", "from", "of", "the", "and", "for", "at", "by"]
+    return (forbiddenWords.includes(word))
+  }
 }
